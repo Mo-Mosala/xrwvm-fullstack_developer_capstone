79d6e4082738388318f17e0afcbe6146902df5e6
Mosala Masoetsa <mosalam365@gmail.com>
2024-03-13 23:20:49 +0200

    Adding Car Model and make

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -81,3 +81,16 @@ def registration(request):
 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_d
+
+# Get cars view
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+
